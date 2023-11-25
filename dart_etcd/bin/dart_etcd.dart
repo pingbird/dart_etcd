0b437d5e1232abf673e1f1b29cbaf033b157e05b
Andre Lipke <andre@tst.sh>
2023-11-25 15:19:43 -0800

    Initial commit

---
+++ dart_etcd/bin/dart_etcd.dart
@@ -0,0 +1,5 @@
+import 'package:dart_etcd/dart_etcd.dart' as dart_etcd;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_etcd.calculate()}!');
+}
---
+++ dart_etcd/test/dart_etcd_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_etcd/dart_etcd.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
